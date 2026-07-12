2a6f84c31e9b07b0344928e468fa6d1b2f64ee3b
edouardpineau2013-ops <edouard.pineau.2013@gmail.com>
2026-07-12 19:31:26 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -23,7 +23,61 @@ function envoyerCommandePrédéfinie(commande) {
             "Content-Type": "application/json"
         },
         body: JSON.stringify({
-            texte: commande
+            texte: (commande)
+        })
+    })
+    .then(r => r.json())
+    .then(data => {
+        document.getElementById("reponse").textContent = "Réponse: " + data.reponse;
+    });
+}
+
+function ouvrirMusique(){
+    document.getElementById("popup-musique").style.display="flex";
+}
+
+function fermerMusique(){
+    document.getElementById("popup-musique").style.display="none";
+}
+
+window.onclick=function(event){
+
+    const popup=document.getElementById("popup-musique");
+
+    if(event.target===popup){
+        fermerMusique();
+    }
+
+}
+
+function ouvrirMeteo(){
+    document.getElementById("popup-meteo").style.display="flex";
+}
+
+function fermerMeteo(){
+    document.getElementById("popup-meteo").style.display="none";
+}
+
+window.onclick=function(event){
+
+    const popup=document.getElementById("popup-meteo");
+
+    if(event.target===popup){
+        fermerMeteo();
+    }
+
+}
+
+function envoyerMeteo() {
+    const ville = document.getElementById("ville").value;
+
+    fetch("https://api.gogekko.fr/commande", {        
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            texte: "quelle est la météo à " + ville
         })
     })
     .then(r => r.json())
